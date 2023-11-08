c57979f60b4ba378aba3830cb98450cef95987db
dinhhuyhuy <130577756+dinhhuyhuy@users.noreply.github.com>
2023-11-08 16:05:56 +0700

    Rename "layout.hbs" -> "userlayout.hbs"
    Rename "layout.hbs" -> "userlayout.hbs"
    Change home/index.js
    Chang product/index.js

--- components/home/index.js
+++ components/home/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/index');
+  res.render('home/index', {layout: 'userlayout.hbs'});
 });
 
 module.exports = router;
--- components/product/index.js
+++ components/product/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('product/index');
+  res.render('product/index', {layout: 'userlayout.hbs'});
 });
 
 module.exports = router;
