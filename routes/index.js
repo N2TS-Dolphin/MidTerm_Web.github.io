3f7c1665bdd8cc3b5bdb2ecf09e10afa954865db
N2TS-Dolphin <sonsung2003@gmail.com>
2023-11-08 16:15:27 +0700

    add categories and checkout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/home", function (req, res, next) {
+  res.render("home/index");
 });
 
+router.get("/product", function (req, res, next) {
+  res.render("product/index");
+});
+
+router.get("/categories", function (req, res, next) {
+  res.render("categories/index");
+});
+
+router.get("/store", function (req, res, next) {
+  res.render("store/index");
+});
+
+router.get("/checkout", function (req, res, next) {
+  res.render("checkout/index");
+});
 module.exports = router;
